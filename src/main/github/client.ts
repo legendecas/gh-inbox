bc4789692ae0c850a8eaa8ba89bb2fb481bb88b9
Chengzhong Wu <legendecas@gmail.com>
2025-08-07 21:03:15 +0100

    Add system CA support

--- src/main/github/client.ts
+++ src/main/github/client.ts
@@ -1,17 +1,37 @@
 import { Octokit } from "@octokit/core";
-import { ProxyAgent, fetch as undiciFetch } from "undici";
+import { getCACertificates, rootCertificates } from "node:tls";
+import { Agent, Dispatcher, ProxyAgent, fetch as undiciFetch } from "undici";
 
 export class GitHubClient {
   private octokit: Octokit;
 
   constructor(baseUrl: string, token: string, proxyUrl?: string | null) {
-    let fetchFn: typeof undiciFetch = undiciFetch;
+    const combinedCACertificates = [
+      ...getCACertificates("system"),
+      ...rootCertificates,
+    ];
+
+    let agent: Dispatcher;
     if (proxyUrl) {
-      const agent = new ProxyAgent(proxyUrl);
-      fetchFn = (url, options) => {
-        return undiciFetch(url, { ...options, dispatcher: agent });
-      };
+      agent = new ProxyAgent({
+        uri: proxyUrl,
+        proxyTls: {
+          ca: combinedCACertificates,
+        },
+        requestTls: {
+          ca: combinedCACertificates,
+        },
+      });
+    } else {
+      agent = new Agent({
+        connect: {
+          ca: combinedCACertificates,
+        },
+      });
     }
+    const fetchFn: typeof undiciFetch = (url, options) => {
+      return undiciFetch(url, { ...options, dispatcher: agent });
+    };
     this.octokit = new Octokit({
       auth: token,
       baseUrl,
